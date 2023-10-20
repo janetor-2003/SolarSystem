ccc5ea8cf14da3a75f997d9dea9e465a56d11228
janetor-2003 <j.anetor@lancaster.ac.uk>
2023-10-20 13:28:48 +0100

    moon orbit fixed

--- Driver.java
+++ Driver.java
@@ -3,22 +3,22 @@ public class Driver {
 
         SolarSystem window = new SolarSystem(1000, 1000);
         Sun sun = new Sun(150, window, "YELLOW");
-        Planet mercury = new Planet(sun, window, 3.3, 3, "GRAY", 100, 60);
-        Planet venus = new Planet(sun, window, 1.1, 9, "DARK GRAY", 125, 45);
-        Planet earth = new Planet(sun, window, 1, 10, "BLUE", 150, 180);
-        Planet mars = new Planet(sun, window, 2, 5, "RED", 200, 270);
-        Planet jupiter = new Planet(sun, window, 0.17, 56, "MAGENTA", 250, 180);
-        Moon earthMoon = new Moon(sun, window, earth, 1, 5, "GRAY", 0, 20);
+        Planet mercury = new Planet(sun, window, 1.1, 3, "GRAY", 200, 60);
+        Planet venus = new Planet(sun, window, 0.6, 9, "DARK GRAY", 250, 45);
+        Planet earth = new Planet(sun, window, 0.5, 10, "BLUE", 300, 180);
+        Planet mars = new Planet(sun, window, 0.5, 5, "RED", 400, 270);
+        Planet jupiter = new Planet(sun, window, 0.17, 56, "MAGENTA", 500, 180);
+        Moon earthMoon = new Moon(sun, window, earth, 1, 5, "GRAY", 0, 5);
 
         while (true) {
             earth.updatePosition(window);
             sun.updatePosition(window);
-            earthMoon.updatePosition(window);
+            //earthMoon.updatePosition(window);
             earthMoon.updatePlanetPosition(window);
-            // mercury.updatePosition(window);
-            // mars.updatePosition(window);
-            // venus.updatePosition(window);
-            // jupiter.updatePosition(window);
+             mercury.updatePosition(window);
+             mars.updatePosition(window);
+             venus.updatePosition(window);
+             jupiter.updatePosition(window);
             window.finishedDrawing();
         }
     }
--- Moon.java
+++ Moon.java
@@ -2,6 +2,8 @@ public class Moon extends Planet {
 
     private double angle_planet;
     private int distance_from_planet;
+    private int x;
+    private double y = 180;
 
     public Moon(Sun orbit_sun, SolarSystem window, Planet parent, double speed, int diameter, String colour,
             double angle_planet, int distance_from_planet) {
@@ -9,14 +11,17 @@ public class Moon extends Planet {
                 parent.getDistanceFromSun() + distance_from_planet, parent.getAngle());
         this.angle_planet = angle_planet;
         this.distance_from_planet = distance_from_planet;
+        this.x = parent.getDistanceFromSun();
+        //this.y = parent.getAngle();
 
-        // window.drawSolarObject(distance_from_planet, angle_planet, diameter, colour);
+        //window.drawSolarObjectAbout(x+distance_from_planet,100,50,colour,100,100);
 
     }
 
     public void updatePlanetPosition(SolarSystem window) {
-        angle_planet += 5;
-        window.drawSolarObjectAbout(10, angle_planet, 5, "GRAY", 150, 0);
+        angle_planet += 3;
+        y += 0.5;
+        window.drawSolarObjectAbout(x+distance_from_planet,y,5,"GRAY",25,angle_planet);
     }
 
 }
